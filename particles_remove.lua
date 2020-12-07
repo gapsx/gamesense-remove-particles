eebb11efcedfafa48955663d5bf9e9f160782cc1
Gabriel <68157259+heygaps@users.noreply.github.com>
2020-12-07 17:14:23 -0300

    fixed "particles_checkbox"  function

--- particles_remove.lua
+++ particles_remove.lua
@@ -1,9 +1,9 @@
 client.log("remove particles loaded")
 
-local sangue_checkbox = ui.new_checkbox("MISC", "Miscellaneous", "Remove particles") 
+local particles_checkbox = ui.new_checkbox("MISC", "Miscellaneous", "Remove particles") 
 
 client.set_event_callback("setup_command", function(e)
-    if ui.get(sangue_checkbox) then
+    if ui.get(particles_checkbox) then
 		client.exec("r_cleardecals")
 	end
 end)
