11ab5987ea7a1beb0c8e19a094b34dc83f92cc3c
Gabriel <68157259+heygaps@users.noreply.github.com>
2020-12-07 14:27:21 -0300

    Add files via upload

---
+++ particles_remove.lua
@@ -0,0 +1,9 @@
+client.log("remove particles loaded")
+
+local sangue_checkbox = ui.new_checkbox("MISC", "Miscellaneous", "Remove particles") 
+
+client.set_event_callback("setup_command", function(e)
+    if ui.get(sangue_checkbox) then
+		client.exec("r_cleardecals")
+	end
+end)
